a744717f8d76b7e1ea96e8b4a6f955cff5bfbdcd
ZhayaGT <jonathanemmanuel0107@gmail.com>
2026-09-22 19:08:47 +0700

    GameVault : Menyelesaikan main feature

--- VercelWeb/gamevault/assets/js/game.js
+++ VercelWeb/gamevault/assets/js/game.js
@@ -1,24 +1,5 @@
 const KOLOM_TABEL_GAME = 6;
 
-function teksAman(nilai) {
-    return String(nilai)
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
-}
-
-function bintang(nilai) {
-    let isi = "";
-    for (let i = 1; i <= 5; i++) {
-        if (i <= Math.round(nilai)) {
-            isi += "★";
-        } else {
-            isi += '<span class="kosong">★</span>';
-        }
-    }
-    return '<span class="bintang">' + isi + "</span> " + Number(nilai).toFixed(1);
-}
-
 function renderTabelGame(daftarGame) {
     const tbody = document.querySelector("#tabel-game tbody");
     if (!tbody) return;
